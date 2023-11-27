cc69a0d9741617aa213aab1cf5a11103c3b6ed55
Edwin Damanik <edwindamanik5@gmail.com>
2023-11-27 11:29:06 +0700

    new update

--- src/components/Musics/Musics.js
+++ src/components/Musics/Musics.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import './Musics.css'
-import { MeditationImage } from '../../assets/images'
+import React, { useState } from "react";
+import "./Musics.css";
+import { MeditationImage } from "../../assets/images";
+import Sound from "react-sound";
+import audioFile from "../../assets/musics/arcade-music.wav";
 
 const Musics = () => {
-  return (
-    <div className='card-music'>
-        <div className='image-music-wrapper'>
-        <img src={MeditationImage} alt='' />
-        </div>
-        <div className='button-music-wrapper'>
-        <button type='button'>Dengarkan</button>
-        </div>
-    </div>
-  )
-}
+  const [isPlaying, setPlaying] = useState(false);
 
-export default Musics
+  const handleButtonClick = () => {
+    setPlaying(!isPlaying);
+  };
+
+  return (
+    <div className="card-music">
+      <div className="image-music-wrapper">
+        <img src={MeditationImage} alt="" />
+      </div>
+      <div className="button-music-wrapper">
+        <button type="button" onClick={handleButtonClick}>
+          {isPlaying ? "Stop" : "Dengarkan"}
+        </button>
+      </div>
+      <Sound
+        url={audioFile}
+        playStatus={isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED}
+        onFinishedPlaying={() => setPlaying(false)}
+      />
+    </div>
+  );
+};
+
+export default Musics;
