805310c553cfbe153555047580ec6f89da54e252
Edwin Damanik <edwindamanik5@gmail.com>
2023-11-28 10:42:15 +0700

    update 404 not found page

--- src/container/pages/NotFound/NotFound.js
+++ src/container/pages/NotFound/NotFound.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div id="notfound">
+      <div class="notfound">
+        <div class="notfound-404">
+          <h1>Oops!</h1>
+          <h2>404 - The Page can't be found</h2>
+        </div>
+        <a href="/">Go TO Homepage</a>
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
